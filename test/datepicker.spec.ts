7da6f2d772760a8e53d0d41728cc69fdcf402c9d
Ashish Maithani <ashishmaithani13981@gmail.com>
2026-03-15 15:37:33 +0530

    Update tests

--- test/datepicker.spec.ts
+++ test/datepicker.spec.ts
@@ -3,13 +3,11 @@ import { text } from 'node:stream/consumers';
 
 
 test('handle iframe and dropdown In playwright', async ({ page }) => {
-    await page.goto('https://jqueryui.com/droppable/', { timeout: 60000 });
+    await page.goto('https://jqueryui.com/datepicker/', { timeout: 60000 });
 
     const iframe = page.frameLocator('.demo-frame');
-    const draggable = iframe.locator('#draggable');
-    const droppable = iframe.locator('#droppable');
-    await draggable.dragTo(droppable);
-    // await expect(droppable).toHaveText('Dropped!');
+
+    await iframe.locator('#datepicker').fill('');
 
 
 
